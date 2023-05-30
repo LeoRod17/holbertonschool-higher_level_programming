01ccdf2c52d004ca4da8e02b5482aa21b66dac76
root <matiassotelo171097@gmail.com>
2023-05-30 05:48:48 -0700

    5-print comb v1

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+txt1 = "{}, "
+txt2 = "{}\n"
+for number in range(0, 100):
+    if(number <= 98):
+        print(txt1.format(str(number).zfill(2)), end="")
+    else:
+        print(txt2.format(number), end="")
+        
