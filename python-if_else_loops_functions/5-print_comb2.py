b1d7eaceb7e25ed5e58ea656d4b1ff1a57274092
root <matiassotelo171097@gmail.com>
2023-05-30 05:51:56 -0700

    5-print comb v2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,8 +2,7 @@
 txt1 = "{}, "
 txt2 = "{}\n"
 for number in range(0, 100):
-    if(number <= 98):
+    if number <= 98:
         print(txt1.format(str(number).zfill(2)), end="")
     else:
         print(txt2.format(number), end="")
-        
