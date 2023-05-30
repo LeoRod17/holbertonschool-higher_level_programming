16986b2d95ab5b8e932fd44ba42088a723b9f5e5
root <matiassotelo171097@gmail.com>
2023-05-30 07:15:39 -0700

    6-print combo v1

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+txt1 = "{}{}, "
+txt2 = "{}{}\n"
+for number in range(0, 10):
+    for num in range(number + 1, 10):
+        if number == 8 and num == 9:
+            print(txt2.format(number, num), end="")
+        else:
+            print(txt1.format(number, num), end="")
