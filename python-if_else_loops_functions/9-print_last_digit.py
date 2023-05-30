e80546d669f33c7ad1e1c7732257108c3736e7a8
root <matiassotelo171097@gmail.com>
2023-05-30 10:08:55 -0700

    fixing errors

--- python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-def print_last_digit(number):
-    txt = "{}"
-    if number < 0:
-        number = number * -1
-    num = number % 10
-    print(txt.format(num), end="")
-    return (num)
