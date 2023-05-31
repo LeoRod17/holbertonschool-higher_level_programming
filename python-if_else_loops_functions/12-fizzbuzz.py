86f1966f1abe39a5d5da29396333743121f19a72
root <matiassotelo171097@gmail.com>
2023-05-31 05:09:48 -0700

    12-fizzbuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    txt = "{} "
+    for num in range(0, 101):
+        if num % 3 == 0:
+            print(txt.format("Fizz"), end="")
+        elif num % 5 == 0:
+            print(txt.format("Buzz"), end="")
+        elif num % 3 == 0 and num % 5 == 0:
+            print(txt.format("FizzBuzz"), end="")
+        else:
+            print(txt.format(num), end="")
