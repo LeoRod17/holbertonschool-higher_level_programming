8dc441a13435fd4f6c49a8cef8529b04ad4d907e
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-01 11:39:37 -0700

    2-args v4

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,13 @@
 if __name__ == "__main__":
     import sys
     txt = "arguments"
+    txt2 = "argument"
     count = len(sys.argv)
     if (count == 1):
         print("{} {}.".format(count - 1, txt))
+    elif count == 2:
+        print("{} {}:".format(count - 1, txt2))
+        print("{}: {}".format(count - 1, sys.argv[1]))
     else:
         print("{} {}:".format(count - 1, txt))
         for s in range(1, count):
