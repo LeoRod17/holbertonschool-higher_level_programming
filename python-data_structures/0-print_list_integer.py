9be0af71c510d176368c97e4756ec2542c86835d
root <matiassotelo171097@gmail.com>
2023-06-05 05:42:52 -0700

    0-print v1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 def print_list_integer(my_list=[]):
     txt = "{}"
-    if my_list == 0:
-        return -1
     size = len(my_list)
     for x in range(size):
         print(txt.format(my_list[x]))
