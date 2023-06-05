351d11744eb83cf85246153b4706fdd9432da7e8
root <matiassotelo171097@gmail.com>
2023-06-05 06:07:02 -0700

    1-element v3

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    txt = "{}"
     size = len(my_list)
     if (idx <= size and idx >= 0):
         return my_list[idx]
-    return None
+    else: 
+        return None
