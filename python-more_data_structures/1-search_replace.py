a48d0e3b9f9a7b7674565064490d22052a2a69ab
root <matiassotelo171097@gmail.com>
2023-06-06 07:17:06 -0700

    1-search v2

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if (my_list):
+        lista = my_list.copy()
         for x in range(0, len(my_list)):
             if my_list[x] == search:
-                my_list[x] = replace
+                lista[x] = replace
+    return lista
