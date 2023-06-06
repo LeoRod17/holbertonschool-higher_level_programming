3666a17cf3b9616c978000db2206b7500d9233c2
root <matiassotelo171097@gmail.com>
2023-06-06 05:14:00 -0700

    9-maxval v5

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    val1
     if my_list:
-        for x in range(0, len(my_list)):
-            if my_list[x] > val1:
+        val1 = 0
+        size = len(my_list)
+        my_list.sort()
+        for x in range(0, size):
+            if x == size - 1:
                 val1 = my_list[x]
         return ("{:d}".format(val1))
     else:
