5639c90b41572382d5664b0ad3e0d9f7ea51b053
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-08 15:32:55 -0700

    2-safePrint

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for a in range(x):
+        try:
+            print("{:d}".format(my_list[a]), end="")
+            count = count + 1
+        except IndexError:
+            break
+        except ValueError:
+            break
+        except TypeError:
+            break
+    print()
+    return count
