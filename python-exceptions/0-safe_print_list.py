08e78f71148fe2ce7bec599ee6e7e93e4c7b8795
root <matiassotelo171097@gmail.com>
2023-06-08 09:43:53 -0700

    0-safe print v2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for a in range(0, x):
-            print("{}".format(my_list[a]), end="")
+            print("{:d}".format(my_list[a]), end="")
             count = count + 1
         print()
         return count
