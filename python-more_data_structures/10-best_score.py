7e81dcde41ef455c3dc1f3fb85253789f8aa4d3c
root <matiassotelo171097@gmail.com>
2023-06-08 07:02:00 -0700

    10-best

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if (a_dictionary != None):
+        v = a_dictionary.values()
+        m = max(v)
+        for x in a_dictionary:
+            if (m == a_dictionary.get(x)):
+                return x
+    return None
