a45137a9f2285b53c709473f0196d216ee1699d7
root <matiassotelo171097@gmail.com>
2023-06-08 07:27:14 -0700

    10-best v3

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if (a_dictionary != None):
+    if a_dictionary:
         v = a_dictionary.values()
         m = max(v)
         for x in a_dictionary:
             if (m == a_dictionary.get(x)):
                 return x
-        return None
     return None
