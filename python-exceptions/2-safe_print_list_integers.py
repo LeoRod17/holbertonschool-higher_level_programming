129fae44f1d7ba43b9aec32700725a21d8e15958
root <matiassotelo171097@gmail.com>
2023-06-09 05:07:47 -0700

    2-safeprint v2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             break
         except ValueError:
-            break
+            continue
         except TypeError:
             break
     print()
