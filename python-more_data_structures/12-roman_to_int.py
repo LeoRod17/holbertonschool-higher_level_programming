4117b376a546088901477f15faa3115a3ad976d7
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-10 10:27:58 -0700

    12-roman converter v2

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,13 @@
 def roman_to_int(roman_string):
-    dic = {"I": 1, "V": 5, "X": 10,"L": 50, "C": 100, "D": 500, "M": 1000}
-    rom = roman_string
-    for x in range(len(roman_string) - 1):
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rom = [0] * len(roman_string)
+    for x in range(len(roman_string)):
         for r in dic:
-            if x == dic[r]:
+            if roman_string[x] == r:
                 rom[x] = dic.get(r)
     val = rom[0]
-    for s in rom:
-        if rom[x] < rom[x + 1]:
-            val = val - rom[x + 1]
-        else:
-            val = val + rom[x + 1]
+    for s in range(1, len(rom)):
+        if rom[s] > rom[s - 1]:
+            val = val - rom[s - 1] - rom[s - 1]
+        val = val + rom[s]
     return val
