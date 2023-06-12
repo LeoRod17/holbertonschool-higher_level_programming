8588ac7f5cb8341a0d5e0f61235cb17b5fc436eb
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-12 09:25:08 -0700

    0-i really should have reed the requierements v3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'
+this is a task for holberton school were i have to creat an empty class and nothing more and also document what i am doing here
+because we are starting working with OOP(Object Oriented Programming)
+"""
 class Square:
     pass
-s = Square()
+"""(python3 -c 'print(__import__("my_module").MyClass.__doc__)')
+This is an empty class of a square to practise in how we create a empty class without methods and attributes
+"""
