53ae654a63e449e479eb6e47f42ccddb689478da
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-14 15:12:35 -0700

    4-print square Test v2

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'
+A function that prints a square with the character #.
+"""
 def print_square(size):
+    """python3 -c 'print(__import__("my_module").my_function.__doc__)
+    a function that prints a square while checking it`s
+    value and type
+    """
     if type (size) is float and size < 0:
         raise TypeError("size must be an integer")
     if type (size) is not int:
