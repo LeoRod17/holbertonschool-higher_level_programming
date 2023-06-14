c316ae6d8ebb884034eedd41f45a2d89337c33c9
root <matiassotelo171097@gmail.com>
2023-06-14 05:54:37 -0700

    0-addtest v1

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'
+this is proyect is to learn how to make chekers and tests to try
+"""
+
+
 def add_integer(a, b=98):
+    """python3 -c 'print(__import__("my_module").my_function.__doc__)
+    a function that adds two ints and sends a error mesage if not an int or float
+    converts floats into ints 
+    """
+
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
@@ -9,8 +19,3 @@ def add_integer(a, b=98):
     if not isinstance(b, int):
         raise TypeError("b must be an integer")
     return (a + b)
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
