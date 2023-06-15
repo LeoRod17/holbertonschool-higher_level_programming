92b0b6d4be19a10a447018ff4a79681fb57ac531
root <matiassotelo171097@gmail.com>
2023-06-15 06:53:46 -0700

    5- forgot to comment :(

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+""" a function that prints a text with new lines after this
+characters: (.), (?) and (:)
+"""
 def text_indentation(text):
+    """the text will separate and print each time a special
+    symbol appears
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     txt = ""
-    for x in range (len(text)):
+    token = 0
+    for x in range(len(text)):
         txt = txt + text[x]
-        if text[x] == "." or text[x] == "?" or text[x] == ":":
+        if text[x] == "." or text[x] == "?" or text[x] == ":" and token == 0:
             while text[x + 1] == " ":
                 x = x + 1
             print("{}".format(txt))
             print()
             txt = ""
-            
     print("{}".format(txt))
