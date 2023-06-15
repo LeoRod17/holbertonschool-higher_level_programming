2d28eec71d941154e6d11bee92d377ffe538b723
root <matiassotelo171097@gmail.com>
2023-06-15 06:05:22 -0700

    5-text indentation test

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    txt = ""
+    for x in text:
+        txt = txt + x
+        if x == "." or x == "?" or x == ":":
+            print("{}".format(txt))
+            print()
+            txt = ""
+    print("{}".format(txt))
+        
