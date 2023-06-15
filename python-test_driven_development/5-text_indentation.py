08beb0b5d458c4573a7a146412a71509994cd9cb
root <matiassotelo171097@gmail.com>
2023-06-15 06:31:52 -0700

    5-text indentation test v3

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,10 +3,13 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     txt = ""
-    for x in text:
-        txt = txt + x
-        if x == "." or x == "?" or x == ":":
+    for x in range (len(text)):
+        txt = txt + text[x]
+        if text[x] == "." or text[x] == "?" or text[x] == ":":
+            while text[x + 1] == " ":
+                x = x + 1
             print("{}".format(txt))
             print()
             txt = ""
+            
     print("{}".format(txt))
