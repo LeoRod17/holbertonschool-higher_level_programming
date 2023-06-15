de49011f85178e8df5de6a49d507cd3364adb0b0
root <matiassotelo171097@gmail.com>
2023-06-15 09:52:43 -0700

    1-rectangle get set v3

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,13 +6,14 @@ class Rectangle:
     """it creates a class called Rectangle"""
     def __init__(self, width=0, height=0):
         """it creates the prototype of the class square"""
-        self.__width = width
         self.__height = height
-
+        self.__width = width
+        
+    @property
     def width(self):
         """getter of width"""
         return self.__width
-
+    @width.setter
     def width(self, value):
         """setter of width"""
         if type(value) is not int:
@@ -21,11 +22,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-
+    @property
     def height(self):
         """getter of height"""
         return self.__height
-
+    @height.setter
     def height(self, value):
         """setter of height"""
         if type(value) is not int:
@@ -33,4 +34,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__width = value
+            self.__height = value
