3aa4a1cdf462ff849660ef7a758564a9ea059c7e
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-17 08:32:39 -0700

    6-unicode v3

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,11 +11,18 @@ class TestMaxInteger(unittest.TestCase):
         self.assertTrue(max_integer([1, 2, 4, 3]))
         self.assertTrue(max_integer([4, 1, 4, 4]))
         self.assertTrue(max_integer([-1, -2, -3, -4]))
+        self.assertTrue(max_integer([14, 51, 134, 24]))
+        self.assertTrue(max_integer([500000]))
 
     def testTypeError(self):
         """a function that test if a type error hapens"""
         with self.assertRaises(TypeError):
             max_integer([1, 2, 3, "4"])
+            max_integer(["1", "2", "3", "4"])
+            max_integer("hellow")
+            max_integer(5)
+            max_integer(True)
+            max_integer([1, "2", True, False, "goodbye", 10])
             max_integer()
 
 
