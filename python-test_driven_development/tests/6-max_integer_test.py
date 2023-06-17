a13b13e9b6f2505f234449b021fb39762bb79c36
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-17 08:11:12 -0700

    6-unitest v1

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,11 +1,20 @@
 import unittest
+max_integer = __import__('6-max_integer').max_integer
+"""Working with unicode with a function created by holberton school"""
 
-class TestStringMethods(unittest.TestCase):
-    def testNull(self):
-        self.assertIsNone()
-    def testInt(self):
-        self.assertIsInstance(int)
-        
+class TestMaxInteger(unittest.TestCase):
+    """I check different cases of errors"""
+
+    def testNormal(self):
+        """normal test of how it should work normaly"""
+        self.assertTrue(max_integer([1, 2, 3, 4]))
+        self.assertTrue(max_integer([1, 2, 4, 3]))
+
+    def testTypeError(self):
+        """a function that test if a type error hapens"""
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, 3, "4"])
+            max_integer()
 
 
 if __name__ == '__main__':
