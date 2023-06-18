2aa1bf9289900d4df7031b946ff01f69dd411381
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-18 14:32:57 -0700

    I feel terible and i will upload what i have done

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     mat = [0] * len(matrix)
-    if  type(div) is not int or type(div) is not float:
+    if  type(div) is  int or type(div) is float:
+        if div == 0 :
+            raise ZeroDivisionError("division by zero")
+        for x in range(len(matrix)):
+                mat[x] = list(map(lambda y: y / div, matrix[x]))
+    else:
         raise TypeError("div must be a number")
-    if div == 0 :
-        raise ZeroDivisionError("division by zero")
-    for x in range(len(matrix)):
-        for j in range(len(matrix[x])):
-           if type(matrix[j]) is not int or type(matrix[j]) is not float :
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            
-    return mat
-                
+    return mat 
                 
