bdc2fbbb93436cc477919233ff4c57bb9f30b05c
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-24 09:40:43 -0700

    10-square v1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Creating a Child of Rectangle class that is a
+child of base_geometry called Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """the class square with width and height are the same
+    in one value called size"""
+    def __init__(self, size):
+        super().__init__(size, size)
