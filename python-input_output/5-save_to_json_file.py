43d65c3c4cbdb208e4e1b02f24eef77008adfd32
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-24 16:05:06 -0700

    5-save_to_json v2

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """writes a file for a given object an remakes the file if it not exists"""
     with open(filename, mode="w+") as f:
         json.dump(my_obj, f)
