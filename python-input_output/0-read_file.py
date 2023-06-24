778528625d623f3b97fe6776904317a294e29f59
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-24 08:10:47 -0700

    0-read v5

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """a function that reads a text file (UTF8) and prints it to stdout:"""
 
+
 def read_file(filename=""):
     """a function that reads a text file and prints it"""
-    with open(filename, 'r') as f:
-       for x in f:
-           print(x, end ="")
-    print()
+    with open(filename) as f:
+        print(f.read())
