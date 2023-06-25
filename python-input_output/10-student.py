a418ca860e5906918182124a989c4268f1fcd340
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-25 16:32:24 -0700

    i did my best

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a class Student that defines a student by:"""
+
+
+class Student:
+    """The class Student which includes a first name, last name and age"""
+    def __init__(self, first_name, last_name, age):
+        """the first funtion that it will run when creating a class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list):
+            for y in attrs:
+                for x in self.__dict__:
+                    if y == x:
+                        return vars(self)[y]
+        else:
+            return self.__dict__
