b27af76140c9f870ffe95270f3cd5643b7dd2878
LeoRod17 <matiassotelo171097@gmail.com>
2023-06-25 10:11:00 -0700

    8-class pycode fix

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@ with simple data structure (list, dictionary, string, integer
 and boolean) for JSON serialization of an object:
 """
 
+
 def class_to_json(obj):
     """the unworking function"""
     return obj.__dict__
