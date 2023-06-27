e8987873e5bee9fe562e703e7627305e8b5b436e
root <matiassotelo171097@gmail.com>
2023-06-27 06:53:47 -0700

    square v2.5

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,3 +21,28 @@ class Square(Rectangle):
         txt = "[Square] ({}) {}/{} - {}"
         rec = txt.format(self.id, self.x, self.y, self.width)
         return rec
+
+    def update(self, *args, **kwargs):
+         """a function that updates the Square"""
+         if args:
+                if len(args) > 0:
+                    self.id = args[0]
+                if len(args) > 1:
+                    self.width = args[1]
+                    self.height = args[1]
+                if len(args) > 2:
+                    self.x = args[2]
+                if len(args) > 3:
+                    self.y = args[3]
+         else:
+            for x, y in kwargs.items():
+                if x == "id":
+                    self.id = y
+                if x == "size":
+                    self.height = y
+                    self.width = y
+                if x == "x":
+                    self.x = y
+                if x == "y":
+                    self.y = y
+
