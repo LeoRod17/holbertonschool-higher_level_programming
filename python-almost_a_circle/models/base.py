debd92600cba552f5b3174f3e1519f24f3d87085
root <matiassotelo171097@gmail.com>
2023-06-29 05:22:19 -0700

    15-base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Write the first class Base:"""
-
+import json
 
 class Base():
     """The base class"""
@@ -12,3 +12,10 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return str(list_dictionaries)
