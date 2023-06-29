669e93e292862ca577469da5faac7485de97a9c7
root <matiassotelo171097@gmail.com>
2023-06-29 05:32:28 -0700

    15-base v3

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,7 +16,8 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None:
             return "[]"
         else:
-           return json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)
