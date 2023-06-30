2a87c34b10a372bdd1c71bb37a0dc64b2e763bef
root <matiassotelo171097@gmail.com>
2023-06-30 12:35:01 -0700

    test rectangle & square v3

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -14,9 +14,15 @@ class TestRectangle(unittest.TestCase):
         self.assertAlmostEqual(r1.x, 3)
         self.assertAlmostEqual(r1.y, 2)
         with self.assertRaises(ValueError):
-            r2 = Rectangle(0, 0, -1, -2, 5)
+            r2 = Rectangle(0, 1, 1, 2, 5)
+            r7 = Rectangle(1, 0, 3, 4, 5)
+            r8 = Rectangle(5, 6, -1, 4, 2)
+            r9 = Rectangle(6, 3, 4, -6, 2)
         with self.assertRaises(TypeError):
-            r3 = Rectangle("A", "B", "C", "E", 6)
+            r3 = Rectangle("A", 1, 2, 3, 6)
+            r4 = Rectangle(1, "A", 2, 3, 4)
+            r5 = Rectangle(1, 2, "A", 3, 4)
+            r6 = Rectangle(1, 2, 3, "A", 4)
 
     def testArea(self):
         r1 = Rectangle(2, 4)
@@ -34,6 +40,13 @@ class TestRectangle(unittest.TestCase):
         r1.update(id=5, width=6, height=3, x=2, y=1)
         self.assertAlmostEqual(Rectangle.__str__(r1),
                                "[Rectangle] (5) 2/1 - 6/3")
+        r2 = Rectangle(2, 4, 5, 6, 7)
+        self.assertAlmostEqual(Rectangle.__str__(r2),
+                               "[Rectangle] (7) 5/6 - 2/4")
+        r2.update(5, 6, 3, 2)
+        self.assertAlmostEqual(Rectangle.__str__(r2),
+                               "[Rectangle] (5) 2/6 - 6/3")
+
 
     def testDictionary(self):
         r1 = Rectangle(2, 4, 5, 6, 7)
