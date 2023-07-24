1bac948e24d9ea9738ffca71070d8303fac7ff95
root <matiassotelo171097@gmail.com>
2023-07-24 10:06:48 -0700

    SQL task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Creating a second table
+
+CREATE TABLE if not EXISTS description (
+    id int, name varchar(256), score int 
+);
+
+INSERT INTO description(id, name, score) VALUE (1, 'John', 10);
+INSERT INTO description(id, name, score) VALUE (2, 'Alex', 3);
+INSERT INTO description(id, name, score) VALUE (3, 'Bob', 14);
+INSERT INTO description(id, name, score) VALUE (4, 'George', 8);
