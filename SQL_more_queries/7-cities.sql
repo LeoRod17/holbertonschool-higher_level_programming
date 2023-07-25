72e6800a7d1ceb1d17bf6309ae74a2ee5f0b5bc9
root <matiassotelo171097@gmail.com>
2023-07-25 09:46:45 -0700

    SQLM: TASK 7 v2

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id int UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
     state_id int NOT NULL,
     name varchar(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
