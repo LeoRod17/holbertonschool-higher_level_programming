3fbedf0d02fb2e59c629447391fcb8d00ed6817c
LeoRod17 <matiassotelo171097@gmail.com>
2023-07-27 15:29:11 -0700

    ORM Task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Write a script that takes in an argument and displays all values in
-the states table of hbtn_0e_0_usa where name matches the argument."""
+"""Wait, do you remember the previous task? Did you test "Arizona';
+TRUNCATE TABLE states ; SELECT * FROM states WHERE name = '" as an input?."""
 import MySQLdb
 import sys
 
