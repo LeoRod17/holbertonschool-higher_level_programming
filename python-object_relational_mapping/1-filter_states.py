0eac12590034da61c4ccdd3d0c18593efadd6f59
root <matiassotelo171097@gmail.com>
2023-07-27 10:23:11 -0700

    ORM Task 0 pycode fix

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa:"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """The code would not be executed when imported"""
+    dbconnection = MySQLdb.Connect("localhost", sys.argv[1],
+                                   sys.argv[2], sys.argv[3])
+    dbcommand = dbconnection.cursor()
+    dbcommand.execute("SELECT * FROM states WHERE LEFT(name,1) = 'N' ORDER BY states.id ASC")
+    res = dbcommand.fetchall()
+    for x in res:
+                print(x)
+    dbconnection.close()
