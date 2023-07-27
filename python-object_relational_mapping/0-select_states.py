fc8a8ae9fe125427d43a67c322f6ac029c4471fd
root <matiassotelo171097@gmail.com>
2023-07-27 09:13:49 -0700

    ORM Task 0 pycode fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,10 @@ import sys
 
 if __name__ == '__main__':
     """The code would not be executed when imported"""
-    dbconnection = MySQLdb.Connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    dbconnection = MySQLdb.Connect("localhost", sys.argv[1],
+                                   sys.argv[2], sys.argv[3])
     dbcommand = dbconnection.cursor()
     dbcommand.execute("SELECT * FROM states ORDER BY states.id ASC")
     for x in dbcommand.fetchall():
         print(x)
     dbconnection.close()
-
