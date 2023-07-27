49059fc195809cbf465f0f0babf0713fd85c29c3
LeoRod17 <matiassotelo171097@gmail.com>
2023-07-27 15:25:36 -0700

    ORM Task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa:"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """The code would not be executed when imported"""
+    dbconnection = MySQLdb.Connect("localhost", sys.argv[1],
+                                   sys.argv[2], sys.argv[3])
+    dbcommand = dbconnection.cursor()
+    dbcommand.execute("{}".format("SELECT * FROM states ORDER BY states.id"))
+    res = dbcommand.fetchall()
+    for x in res:
+        if x[1] == sys.argv[4]:
+            print(x)
+    dbconnection.close()
