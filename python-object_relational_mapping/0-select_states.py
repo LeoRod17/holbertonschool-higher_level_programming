bb0f6a7f20b2b22a7f86513364bd599b002f9fa6
root <matiassotelo171097@gmail.com>
2023-07-27 09:11:02 -0700

    ORM Task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa:"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """The code would not be executed when imported"""
+    dbconnection = MySQLdb.Connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+    dbcommand = dbconnection.cursor()
+    dbcommand.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for x in dbcommand.fetchall():
+        print(x)
+    dbconnection.close()
+
