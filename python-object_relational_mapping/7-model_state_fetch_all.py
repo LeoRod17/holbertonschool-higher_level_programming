36850f457fb306421c437de1da3cae2dc24467fc
LeoRod17 <matiassotelo171097@gmail.com>
2023-07-29 13:17:32 -0700

    OMR Task 7 pycode fix

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,8 @@ from sqlalchemy import create_engine
 if __name__ == '__main__':
     """The code would not be executed when imported"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-           sys.argv[1],sys.argv[2],sys.argv[3]))
-    
+           sys.argv[1], sys.argv[2], sys.argv[3]))
+
     st = sqlalchemy.select(State.id, State.name).order_by(State.id)
     con = engine.connect()
     res = con.execute(st).fetchall()
