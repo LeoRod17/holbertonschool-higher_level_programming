f66e946dd571598cc631b6b795b02df6396be6a8
LeoRod17 <matiassotelo171097@gmail.com>
2023-07-29 13:16:16 -0700

    OMR Task 7 v2

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """a script that lists all State objects from the database hbtn_0e_6_usa"""
-from model_state import Base, State
-import MySQLdb
+from model_state import State
 import sys
+import sqlalchemy
+from sqlalchemy import create_engine
 
 
 if __name__ == '__main__':
     """The code would not be executed when imported"""
-    dbconnection = MySQLdb.Connect("localhost",
-                                   sys.argv[1],
-                                   sys.argv[2],
-                                   sys.argv[3])
-    dbcommand = dbconnection.cursor()
-    dbcommand.execute("SELECT * FROM states")
-
-    dbcommand.fetchall
-    for x in dbcommand.fetchall():
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+           sys.argv[1],sys.argv[2],sys.argv[3]))
+    
+    st = sqlalchemy.select(State.id, State.name).order_by(State.id)
+    con = engine.connect()
+    res = con.execute(st).fetchall()
+    for x in res:
         print("{}: {}".format(x[0], x[1]))
