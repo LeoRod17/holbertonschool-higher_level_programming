208152e1390849f9926df0045aee8ba0c2acec0f
LeoRod17 <matiassotelo171097@gmail.com>
2023-07-28 18:24:34 -0700

    ORM Task 6 v2

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """The class State"""
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, unique=True,
                 primary_key=True, nullable=False)
