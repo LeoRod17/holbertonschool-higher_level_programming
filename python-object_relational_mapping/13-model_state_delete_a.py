183bf9e410ce006b12c2d70d5b3d2610f498addb
LeoRod17 <matiassotelo171097@gmail.com>
2023-07-30 10:36:15 -0700

    ORM: Task 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a script that changes the name of a State
+object from the database hbtn_0e_6_usa"""
+from model_state import State
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    """The code would not be executed when imported"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+           sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    dele = session.query(State).filter(State.name.like('%a%')).delete()
+    session.commit()
